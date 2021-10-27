8704ea40f8a88f05a1ed2cc18c6d95fb7a79beae
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2021-10-27 13:08:31 +0200

    Added some stuff to prepare for exposed side calculations

--- MinecraftDOD/Assets/Scripts/BlockState.cs
+++ MinecraftDOD/Assets/Scripts/BlockState.cs
@@ -1,20 +1,24 @@
 ﻿using Unity.Entities;
+using UnityEngine;
 
 namespace TechnOllieG
 {
 	public enum BlockID
 	{
-		None = -1,
+		Air,
 		Grass,
 		Dirt,
 		Stone,
 		Wood,
 		Leaves
 	}
-	
+
 	[GenerateAuthoringComponent]
 	public struct BlockState : IComponentData
 	{
 		public BlockID id;
+
+		// Bitmask specifying which sides of the block are exposed, 00{-z}{z}{-y}{y}{-x}{x}
+		[HideInInspector] public byte exposedSides;
 	}
 }
--- MinecraftDOD/Assets/Scripts/UtilityLibrary.cs
+++ MinecraftDOD/Assets/Scripts/UtilityLibrary.cs
@@ -12,9 +12,19 @@ namespace TechnOllieG
 			return new int2(x ?? a.x, y ?? a.y);
 		}
 		
+		public static float3 With(this float3 a, float? x = null, float? y = null, float? z = null)
+		{
+			return new float3(x ?? a.x, y ?? a.y, z ?? a.z);
+		}
+		
 		public static int2 WithAdd(this int2 a, int x = 0, int y = 0)
 		{
 			return new int2(a.x + x, a.y + y);
+		}
+		
+		public static float3 WithAdd(this float3 a, float x = 0, float y = 0, float z = 0)
+		{
+			return new float3(a.x + x, a.y + y, a.z + z);
 		}
 
 		public static bool IsNearlyZero(this Vector3 v, float preciseness = SmallNumber)

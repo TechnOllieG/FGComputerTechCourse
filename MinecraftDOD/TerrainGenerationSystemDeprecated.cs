c6534d9a29419956ddfd6d7c13fe2641e2f9297f
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2021-10-27 13:10:10 +0200

    Made copy of local changes to terrain generation system (of stuff that didn't actually work, but I'm gonna base off of that to make a working script)

---
+++ MinecraftDOD/TerrainGenerationSystemDeprecated.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics.GraphicsIntegration;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace TechnOllieG
+{
+	[AlwaysUpdateSystem]
+	[AlwaysSynchronizeSystem]
+	[UpdateInGroup(typeof(SimulationSystemGroup))]
+	public class TerrainGenerationSystem : JobComponentSystem
+	{
+		public static TerrainGenerationSystem Instance;
+
+		private GameData _data;
+		private List<int2> _chunksToGenerate = new List<int2>();
+		private List<int2> _chunksToDegenerate = new List<int2>();
+		private List<int2> _currentlyGeneratedChunks = new List<int2>();
+		private EntityQueryDesc _objectsWithoutCoordinateComponentDesc;
+		private Transform _cameraTf;
+		private bool _currentlyGeneratingChunks = false;
+		private JobHandle _chunkGenerationHandle = default;
+		private List<int2> _chunksScheduledForGeneration = new List<int2>();
+		private NativeMultiHashMap<int2, int3> _blockPositions;
+
+		protected override void OnStartRunning()
+		{
+			if (Instance == null)
+				Instance = this;
+			else
+				Enabled = false;
+			
+			_data = GetSingleton<GameData>();
+
+			_objectsWithoutCoordinateComponentDesc = new EntityQueryDesc
+			{
+				None = new ComponentType[] {typeof(ChunkCoordinate)},
+				All = new ComponentType[] {typeof(Translation), ComponentType.ReadOnly<BlockState>()}
+			};
+
+			_cameraTf = Camera.main.transform;
+
+			_blockPositions = new NativeMultiHashMap<int2, int3>((_data.renderDistance * 2 + 1) * 2 * 256 * 3, Allocator.Persistent);
+		}
+
+		protected override void OnDestroy()
+		{
+			_blockPositions.Dispose();
+		}
+
+		protected override JobHandle OnUpdate(JobHandle inputDeps)
+		{
+			Vector3 cameraPos = _cameraTf.position;
+			DetermineChunksToGenerate(ConvertPositionToChunkCoordinate(cameraPos));
+			GenerateChunks(ref inputDeps);
+			//CalculateExposedQuads(ref inputDeps);
+			DegenerateChunks();
+
+			if (_currentlyGeneratingChunks && _chunkGenerationHandle.IsCompleted)
+			{
+				_chunksScheduledForGeneration.Clear();
+				_currentlyGeneratingChunks = false;
+			}
+
+			return inputDeps;
+		}
+
+		private Mesh MakeQuadMesh()
+		{
+			Vector3[] vertices = new Vector3[4]
+			{
+				new Vector3(0, 0, 0),
+				new Vector3(1, 0, 0),
+				new Vector3(0, 0, 1),
+				new Vector3(1, 0, 1)
+			};
+			
+
+			int[] tris = new int[6]
+			{
+				// lower left triangle
+				0, 2, 1,
+				// upper right triangle
+				2, 3, 1
+			};
+
+			Vector3[] normals = new Vector3[4]
+			{
+				Vector3.up,
+				Vector3.up,
+				Vector3.up,
+				Vector3.up
+			};
+
+			Vector2[] uv = new Vector2[4]
+			{
+				new Vector2(0, 0),
+				new Vector2(1, 0),
+				new Vector2(0, 1),
+				new Vector2(1, 1)
+			};
+
+			Mesh mesh = new Mesh {vertices = vertices, triangles = tris, normals = normals, uv = uv};
+			mesh.Optimize();
+
+			return mesh;
+
+			// Matrix4x4 matrix = Matrix4x4.TRS(new Vector3(0f, 100f, 0f), Quaternion.identity, Vector3.one);
+			// Material material = new Material(Shader.Find("Standard"));
+			// material.enableInstancing = true;
+			//
+			// Graphics.DrawMeshInstanced(mesh, 0, material,new [] {matrix});
+			// Graphics.DrawMeshInstancedProcedural
+		}
+
+		private void CalculateExposedQuads(ref JobHandle dependency)
+		{
+			foreach(int2 chunk in _chunksScheduledForGeneration)
+			{
+				ChunkCoordinate coordinate = new ChunkCoordinate {chunkCoordinate = chunk};
+
+				dependency = Entities.WithSharedComponentFilter(coordinate).ForEach(
+					(ref BlockState blockState, in Translation translation) =>
+					{
+						byte currentBitmask = 0;
+						NativeArray<float3> neighbors = new NativeArray<float3>(6, Allocator.Temp);
+						{
+							neighbors[0] = translation.Value.WithAdd(x: 1);
+							neighbors[1] = translation.Value.WithAdd(x: -1);
+							neighbors[2] = translation.Value.WithAdd(y: 1);
+							neighbors[3] = translation.Value.WithAdd(y: -1);
+							neighbors[4] = translation.Value.WithAdd(z: 1);
+							neighbors[5] = translation.Value.WithAdd(z: -1);
+						}
+
+						for (int i = 0; i < neighbors.Length; i++)
+						{
+							// todo finish
+						}
+					}).Schedule(dependency);
+			}
+		}
+
+		public void SetRenderDistance(int renderDistance)
+		{
+			_data.renderDistance = renderDistance;
+		}
+
+		private void GenerateChunks(ref JobHandle dependency)
+		{
+			if (_chunksToGenerate.Count == 0)
+				return;
+
+			for (int i = 0; i < _chunksToGenerate.Count; i++)
+			{
+				int2 chunk = _chunksToGenerate[i];
+				GenerateChunk(chunk, ref dependency);
+
+				_chunksScheduledForGeneration.Add(chunk);
+			}
+
+			_chunkGenerationHandle = dependency;
+			_currentlyGeneratingChunks = true;
+			_chunksToGenerate.Clear();
+		}
+
+		private void DegenerateChunks()
+		{
+			if (_chunksToDegenerate.Count == 0)
+				return;
+
+			if (_currentlyGeneratingChunks)
+				return;
+
+			foreach (int2 chunk in _chunksToDegenerate)
+			{
+				EntityQuery query = GetEntityQuery(ComponentType.ReadOnly<BlockState>(), ComponentType.ReadOnly<ChunkCoordinate>());
+				query.SetSharedComponentFilter(new ChunkCoordinate {chunkCoordinate = chunk});
+				EntityManager.DestroyEntity(query);
+				_currentlyGeneratedChunks.Remove(chunk);
+				_blockPositions.Remove(chunk);
+			}
+			
+			_chunksToDegenerate.Clear();
+		}
+
+		private void GenerateChunk(int2 chunkCoordinates, ref JobHandle dependency)
+		{
+			int chunkArea = _data.chunkSize * _data.chunkSize;
+			NativeArray<Entity> grassBlocks = EntityManager.Instantiate(_data.grass, chunkArea, Allocator.Temp);
+			NativeArray<Entity> dirtBlocks = EntityManager.Instantiate(_data.dirt, chunkArea * _data.layersOfDirt, Allocator.Temp);
+
+			grassBlocks.Dispose();
+			dirtBlocks.Dispose();
+			
+			int chunkSize = _data.chunkSize;
+			int extents = _data.worldExtents;
+			float xOffset = _data.xOffset;
+			float yOffset = _data.yOffset;
+			float scale = _data.scale * (_data.worldExtents * 2 + 1);
+			float depth = _data.depth;
+			
+			ChunkCoordinate coordinate;
+			coordinate.chunkCoordinate = chunkCoordinates;
+
+			EntityManager.AddSharedComponentData(GetEntityQuery(_objectsWithoutCoordinateComponentDesc), coordinate);
+
+			var blockPositions = _blockPositions.AsParallelWriter();
+
+			dependency = Entities.WithSharedComponentFilter(coordinate)
+				.ForEach((int entityInQueryIndex, ref Translation translation, in BlockState state) =>
+				{
+					int layer = entityInQueryIndex / chunkArea;
+					int blockIndexInChunk = entityInQueryIndex % chunkArea;
+					int2 chunkSpaceBlockCoordinate =
+						new int2(blockIndexInChunk % chunkSize, blockIndexInChunk / chunkSize);
+
+					int2 zeroIndexedChunkCoordinate =
+						new int2(chunkCoordinates.x + extents, chunkCoordinates.y + extents);
+
+					int worldSizeInBlocks = (extents * 2 + 1) * chunkSize;
+					float noiseX = (float) (zeroIndexedChunkCoordinate.x * chunkSize + chunkSpaceBlockCoordinate.x) /
+						worldSizeInBlocks * scale + xOffset;
+					float noiseY = (float) (zeroIndexedChunkCoordinate.y * chunkSize + chunkSpaceBlockCoordinate.y) /
+						worldSizeInBlocks * scale + yOffset;
+
+					int2 chunkCoord2d = chunkCoordinates * chunkSize + chunkSpaceBlockCoordinate;
+
+					int3 blockPos = new int3(chunkCoord2d.x,
+						(int) math.round(Mathf.PerlinNoise(noiseX, noiseY) * depth - layer), chunkCoord2d.y);
+
+					//blockPositions.Add(chunkCoordinates, blockPos);
+
+					translation.Value = blockPos;
+				}).Schedule(dependency);
+			
+			var rigidbodySystem = World.GetExistingSystem<SmoothRigidBodiesGraphicalMotion>();
+			rigidbodySystem.AddInputDependency(dependency);
+			dependency.Complete();
+
+			_currentlyGeneratedChunks.Add(chunkCoordinates);
+		}
+		
+		private void DetermineChunksToGenerate(int2 currentChunk)
+		{
+			int renderDistance = _data.renderDistance;
+			int worldExtents = _data.worldExtents;
+			List<int2> currentChunksToRender = new List<int2> {currentChunk};
+			
+			for (int i = 1; i <= renderDistance; i++)
+			{
+				int startOfCardinals = currentChunksToRender.Count;
+				
+				// Cardinal directions
+				currentChunksToRender.Add(currentChunk.WithAdd(x: i));
+				currentChunksToRender.Add(currentChunk.WithAdd(x: -i));
+				currentChunksToRender.Add(currentChunk.WithAdd(y: i));
+				currentChunksToRender.Add(currentChunk.WithAdd(y: -i));
+
+				int2 cardinal1 = currentChunksToRender[startOfCardinals];
+				int2 cardinal2 = currentChunksToRender[startOfCardinals + 1];
+
+				// Along y axis on both x cardinals
+				bool p = true;
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 1; k <= renderDistance; k++)
+					{
+						currentChunksToRender.Add(cardinal1 + new int2(0, p ? 1 : -1) * k);
+					}
+					
+					for (int k = 1; k <= renderDistance; k++)
+					{
+						currentChunksToRender.Add(cardinal2 + new int2(0, p ? 1 : -1) * k);
+					}
+
+					p = !p;
+				}
+			}
+
+			foreach (int2 current in _currentlyGeneratedChunks)
+			{
+				if (currentChunksToRender.Contains(current))
+					continue;
+				
+				_chunksToDegenerate.Add(current);
+			}
+
+			foreach (int2 current in currentChunksToRender)
+			{
+				if (_currentlyGeneratedChunks.Contains(current))
+					continue;
+
+				if (current.x > worldExtents || current.x < -worldExtents ||
+				    current.y > worldExtents || current.y < -worldExtents)
+					continue;
+				
+				_chunksToGenerate.Add(current);
+			}
+		}
+		
+		int2 ConvertPositionToChunkCoordinate(float3 position)
+		{
+			return new int2((int) math.floor(position.x / _data.chunkSize), (int) math.floor(position.z / _data.chunkSize));
+		}
+	}
+}
